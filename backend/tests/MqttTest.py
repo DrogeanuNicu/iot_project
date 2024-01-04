47e27e6e35319fefbc117f6313bca6717291a594
Drogeanu Nicusor <drogeanunicusor@gmail.com>
2024-01-04 16:14:26 +0200

    updates the mqtt test script

--- backend/tests/MqttTest.py
+++ backend/tests/MqttTest.py
@@ -7,18 +7,9 @@ mqtt_broker = "localhost"
 mqtt_topic = "plant"
 
 while True:
-    message = {
-        "time": int(time.time()),
-        "temp": random.randint(0, 100),
-        "hum": random.randint(0, 100),
-        "moist": random.randint(0, 100),
-        "fan": random.randint(0, 1),
-        "pump": random.randint(0, 1),
-    }
+    message = f"{int(time.time())},{random.randint(0, 100)},{random.randint(0, 100)},{random.randint(0, 100)},{random.randint(0, 1)},{random.randint(0, 1)}"
+    publish.single(mqtt_topic, payload=message, hostname=mqtt_broker)
 
-    json_message = json.dumps(message)
-    publish.single(mqtt_topic, payload=json_message, hostname=mqtt_broker)
-
-    print(f"Published message: {json_message}")
+    print(f"Published message: {message}")
 
     time.sleep(1)
